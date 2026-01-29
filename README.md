65d65e05089f0f40db91fbf199423b283923a010
Emiliabz <emiliabrbz@gmail.com>
2026-01-29 17:31:27 -0300

    Add scripts

---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# script_13.py
+
+Script extraÃ­do do arquivo original.
+
---
+++ script_13.py
@@ -0,0 +1,5 @@
+﻿preÃ§o = float(input('Qual Ã© o preÃ§o do produto? R$ '))
+novo = preÃ§o - (preÃ§o * 5 / 100)
+print('O preÃ§o que custava R${:.2f}, na promoÃ§Ã£o com desconto de 5%, vai custar R${:.2f}'.format(preÃ§o, novo)
+
+      #exercÃ­cio de porcentagem
